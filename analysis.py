f3bc73f76311aa0d74504c5e581e066c81d32a20
David E. Lambert <david@davidelambert.com>
2022-03-17 16:17:44 -0400

    add OLS models and JSON output

--- analysis.py
+++ analysis.py
@@ -1,10 +1,11 @@
 # %%
 from pathlib import Path
+import json
 
 import numpy as np
 import pandas as pd
-from statsmodels.api import OLS
 import plotly.express as px
+import statsmodels.formula.api as smf
 
 ROOT = Path(__file__).parent.resolve()
 d = pd.read_csv(ROOT/'data.csv')
@@ -61,6 +62,8 @@ with open(ROOT/'unit_weights.json', 'a') as f:
 
 # %%[markdown]
 # ## Visualization
+# Double click a material code in the legend to isolate. Single-click a
+# material code to show/hide.
 
 # %%
 mat_map = {
@@ -84,7 +87,8 @@ d['mat_long'] = d['material'].map(mat_map)
 # %%
 p_ps = px.scatter(d[d['material'] == 'ps'], x='gauge_sq', y='unit_weight',
                   trendline='ols', title='Plain Steel Strings',
-                  hover_data={'gauge': True, 'gauge_sq': False, 'brand': True})
+                  hover_data={'gauge': True, 'gauge_sq': False, 'brand': True},
+                  width=600, height=400)
 p_ps.show()
 
 # %%
@@ -93,6 +97,7 @@ elec_filter = [mat in elec_mat for mat in d['material']]
 p_elec = px.scatter(d.loc[elec_filter], x='gauge_sq', y='unit_weight',
                     trendline='ols', title='Electric Wound Strings',
                     hover_data={'gauge': True, 'gauge_sq': False, 'brand': True},
+                    width=600, height=400,
                     color='material', color_discrete_map={'nps': '#008080',
                                                           'ss': '#800080',
                                                           'pn': '#40E0D0',
@@ -105,6 +110,7 @@ acou_filter = [mat in acou_mat for mat in d['material']]
 p_acou = px.scatter(d.loc[acou_filter], x='gauge_sq', y='unit_weight',
                     trendline='ols', title='Acoustic Wound Strings',
                     hover_data={'gauge': True, 'gauge_sq': False, 'brand': True},
+                    width=600, height=400,
                     color='material', color_discrete_map={'pb': '#8B4513',
                                                           '8020': '#FFD700',
                                                           '8515': '#D2691E'})
@@ -116,9 +122,56 @@ bass_filter = [mat in bass_mat for mat in d['material']]
 p_bass = px.scatter(d.loc[bass_filter], x='gauge_sq', y='unit_weight',
                     trendline='ols', title='Bass Strings',
                     hover_data={'gauge': True, 'gauge_sq': False, 'brand': True},
+                    width=600, height=400,
                     color='material', color_discrete_map={'bnps': '#008080',
                                                           'bss': '#800080',
                                                           'bfw': '#EE82EE'})
 p_bass.show()
+
+# %%[markdown]
+# ## OLS modelling
+# Build unit weight models of the form $UW_{i,m} = \alpha_m + \beta_m * gauge_{i}^{2}$
+#
+# Build OLS models for each string material separately for ease of use and
+# interpretation. Write constants and coefficients for each material to JSON for use
+# when unit weights are not available from the lookup table created in the **Mean unit
+# weights** section
+
+# %%
+
+
+def short_table(fitted_model, title=None):
+    """Abbreviated single-variable OLS results table"""
+
+    coef = round(fitted_model.params['gauge_sq'], ndigits=4)
+    se = round(fitted_model.bse['gauge_sq'], ndigits=4)
+    t = round(fitted_model.tvalues['gauge_sq'], ndigits=2)
+    p = round(fitted_model.pvalues['gauge_sq'], ndigits=4)
+    r2 = round(fitted_model.rsquared, ndigits=4)
+
+    cw = 10
+    if title is not None:
+        title = f' {title} '.center(5 * cw, '=')
+    else:
+        title = '=' * (5 * cw)
+    chead = ''.join([ch.rjust(cw) for ch in ['Coef.', 'SE', 't', 'Pr>|t|', 'R^2']])
+    cval = ''.join([str(v).rjust(cw) for v in [coef, se, t, p, r2]])
+    table = '{}\n{}\n{}'.format(title, chead, cval)
+
+    return table
+
+
+# %%
+mat_list = d['material'].unique()
+tension_models = dict()
+for mat in mat_list:
+    mod = smf.ols('unit_weight ~ gauge_sq', data=d[d['material'] == mat]).fit()
+    print(short_table(mod, title=mat_map[mat]), '\n')
+    tension_models[mat] = {'const': mod.params.loc['Intercept'],
+                           'coef': mod.params.loc['gauge_sq']}
+
+# %%
+with open(ROOT/'tension_models.json', 'w') as f:
+    json.dump(tension_models, f, indent=2)
 
 # %%
